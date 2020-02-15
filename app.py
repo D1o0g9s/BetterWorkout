4b304c56f27462df5b23d50d5141b667a8690bfa
D1o0g9s <boodalina@gmail.com>
2020-02-15 12:49:21 -0800

    concat only every 1 second

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ as activity recognition and augmented reality.
 Pose estimation is only supported using the edgeIQ container with an NCS
 accelerator.
 """
-SEC_TO_KEEP = 2
+SEC_TO_KEEP = 5
 MILLIS_TO_KEEP = SEC_TO_KEEP*1000  
 def main():
     pose_estimator = edgeiq.PoseEstimation("alwaysai/human-pose")
@@ -73,8 +73,6 @@ def main():
                         "Inference time: {:1.3f} s".format(results.duration))
                 
                 for ind, pose in enumerate(results.poses):
-
-
                     # Only process pose of person 1
                     if (ind == 0) :
                         prevMillis = millis
@@ -82,84 +80,58 @@ def main():
                         time_elapsed += millis - prevMillis
 
 
-                        right_wrist_y = pose.key_points["Right Wrist"][1]
-                        right_wrist_x = pose.key_points["Right Wrist"][0]
-                        right_elbow_y = pose.key_points["Right Elbow"][1]
-                        right_elbow_x = pose.key_points["Right Elbow"][0]
-                        right_shoulder_y = pose.key_points["Right Shoulder"][1]
-                        right_shoulder_x = pose.key_points["Right Shoulder"][0]
-
-                        left_wrist_y = pose.key_points["Left Wrist"][1]
-                        left_wrist_x = pose.key_points["Left Wrist"][0]
-                        left_elbow_y = pose.key_points["Left Elbow"][1]
-                        left_elbow_x = pose.key_points["Left Elbow"][0]
-                        left_shoulder_y = pose.key_points["Left Shoulder"][1]
-                        left_shoulder_x = pose.key_points["Left Shoulder"][0]
-
-                        user1_right_dict["timestamp"] = [millis]
-                        
-                        user1_right_dict["wrist_y"] = [right_wrist_y]
-                        user1_right_dict["elbow_y"] = [right_elbow_y]
-                        user1_right_dict["shoulder_y"] = [right_shoulder_y]
-
-                        user1_right_dict["wrist_x"] = [right_wrist_x]
-                        user1_right_dict["elbow_x"] = [right_elbow_x]
-                        user1_right_dict["shoulder_x"] = [right_shoulder_x]
-
-
-                        user1_left_dict["timestamp"] = [millis]
-                        user1_left_dict["wrist_y"] = [left_wrist_y]
-                        user1_left_dict["elbow_y"] = [left_elbow_y]
-                        user1_left_dict["shoulder_y"] = [left_shoulder_y]
-
-                        user1_left_dict["wrist_x"] = [left_wrist_x]
-                        user1_left_dict["elbow_x"] = [left_elbow_x]
-                        user1_left_dict["shoulder_x"] = [left_shoulder_x]
-
-                        user1_right_df2 = pd.DataFrame.from_dict(user1_right_dict)
-                        #user1_right_df2.set_index('timestamp', inplace=True)
-                        user1_left_df2 = pd.DataFrame.from_dict(user1_left_dict)
-                        #user1_left_df2.set_index('timestamp', inplace=True)
-
-                        user1_right_df = pd.concat([user1_right_df, user1_right_df2])
-                        user1_left_df = pd.concat([user1_left_df, user1_left_df2])
-
-
-                        ### OOPS 
-
-                        # user1_right_dict["timestamp"].append(millis)
-                        
-                        # user1_right_dict["wrist_y"].append(right_wrist_y)
-                        # user1_right_dict["elbow_y"].append(right_elbow_y)
-                        # user1_right_dict["shoulder_y"].append(right_shoulder_y)
-
-                        # user1_right_dict["wrist_x"].append(right_wrist_x)
-                        # user1_right_dict["elbow_x"].append(right_elbow_x)
-                        # user1_right_dict["shoulder_x"].append(right_shoulder_x)
-
-
-                        # user1_left_dict["timestamp"].append(millis)
-                        # user1_left_dict["wrist_y"].append(left_wrist_y)
-                        # user1_left_dict["elbow_y"].append(left_elbow_y)
-                        # user1_left_dict["shoulder_y"].append(left_shoulder_y)
-
-                        # user1_left_dict["wrist_x"].append(left_wrist_x)
-                        # user1_left_dict["elbow_x"].append(left_elbow_x)
-                        # user1_left_dict["shoulder_x"].append(left_shoulder_x)
-
-                        # Remove any data that is older than 10 seconds 
-                        user1_right_df = user1_right_df[user1_right_df["timestamp"] > (millis - MILLIS_TO_KEEP)]
-                        user1_left_df = user1_left_df[user1_left_df["timestamp"] > (millis - MILLIS_TO_KEEP)]
-                        
-                        if time_elapsed > (1000*2) : 
+                        if time_elapsed > (1000*1) : 
                             time_elapsed = 0
-                            print("right arm data oldest row (should start changing "+str(SEC_TO_KEEP)+"seconds after)")
-                            print(user1_right_df.iloc[0])
-                        text.append("Person {}".format(ind))
-                        text.append('-'*10)
-                        text.append("Key Points:")
-                        for key_point in pose.key_points:
-                            text.append(str(key_point))
+
+                            right_wrist_y = pose.key_points["Right Wrist"][1]
+                            right_wrist_x = pose.key_points["Right Wrist"][0]
+                            right_elbow_y = pose.key_points["Right Elbow"][1]
+                            right_elbow_x = pose.key_points["Right Elbow"][0]
+                            right_shoulder_y = pose.key_points["Right Shoulder"][1]
+                            right_shoulder_x = pose.key_points["Right Shoulder"][0]
+
+                            left_wrist_y = pose.key_points["Left Wrist"][1]
+                            left_wrist_x = pose.key_points["Left Wrist"][0]
+                            left_elbow_y = pose.key_points["Left Elbow"][1]
+                            left_elbow_x = pose.key_points["Left Elbow"][0]
+                            left_shoulder_y = pose.key_points["Left Shoulder"][1]
+                            left_shoulder_x = pose.key_points["Left Shoulder"][0]
+
+                            user1_right_dict["timestamp"] = [millis]
+                            
+                            user1_right_dict["wrist_y"] = [right_wrist_y]
+                            user1_right_dict["elbow_y"] = [right_elbow_y]
+                            user1_right_dict["shoulder_y"] = [right_shoulder_y]
+
+                            user1_right_dict["wrist_x"] = [right_wrist_x]
+                            user1_right_dict["elbow_x"] = [right_elbow_x]
+                            user1_right_dict["shoulder_x"] = [right_shoulder_x]
+
+
+                            user1_left_dict["timestamp"] = [millis]
+                            user1_left_dict["wrist_y"] = [left_wrist_y]
+                            user1_left_dict["elbow_y"] = [left_elbow_y]
+                            user1_left_dict["shoulder_y"] = [left_shoulder_y]
+
+                            user1_left_dict["wrist_x"] = [left_wrist_x]
+                            user1_left_dict["elbow_x"] = [left_elbow_x]
+                            user1_left_dict["shoulder_x"] = [left_shoulder_x]
+
+                            user1_right_df2 = pd.DataFrame.from_dict(user1_right_dict)
+                            #user1_right_df2.set_index('timestamp', inplace=True)
+                            user1_left_df2 = pd.DataFrame.from_dict(user1_left_dict)
+                            #user1_left_df2.set_index('timestamp', inplace=True)
+                            user1_right_df = pd.concat([user1_right_df, user1_right_df2])
+                            user1_left_df = pd.concat([user1_left_df, user1_left_df2])
+
+                            print("right arm data length")
+                            print(len(user1_right_df))
+
+                        # text.append("Person {}".format(ind))
+                        # text.append('-'*10)
+                        # text.append("Key Points:")
+                        # for key_point in pose.key_points:
+                        #     text.append(str(key_point))
 
                 streamer.send_data(results.draw_poses(frame), text)
 
